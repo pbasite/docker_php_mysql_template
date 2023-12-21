3c59ae2a76b7c832f2644599d426620c8e32d1d7
Pascal Bangerter <pascal.bangerter@epfl.ch>
2023-12-21 11:00:01 +0100

    first commit

---
+++ app/index.php
@@ -0,0 +1,9 @@
+<?php
+//---- Pour xDebug, Attention à la variable pathMappings  ----
+//---- du fichier launch.json                             ----
+//---- le projet tourne sous localhost:8090               ----
+$ma_var = 1;
+
+echo $ma_var;
+echo "Hello";
+?>
